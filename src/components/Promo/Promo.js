9df0980ad0ed26cc2e23743cd1bcda215cb5212e
N. Lazuta <nkgavshina@gmail.com>
2022-03-09 20:28:44 +0300

    feat: добавить стили для компонента Promo

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -4,15 +4,16 @@ import "./Promo.scss";
 export default function Promo() {
   return (
     <section className="promo" id="overview">
-      <div className="promo__background"></div>
-      <h1 className="promo__heading">CloudBudget</h1>
-      <h2 className="promo__subtitle">
-        Cloud budget management for everyone. Only €69.95 a Month After a 7 Day
-        Trial of Up to €4.99
-      </h2>
-      <button className="promo__sign-up-button" type="button">
-        Sign Up
-      </button>
+      <div className="promo__background">
+        <h1 className="promo__heading">CloudBudget</h1>
+        <h2 className="promo__subtitle">
+          Cloud budget management for everyone. Only €69.95 a Month After a 7
+          Day Trial of<br />Up to €4.99
+        </h2>
+        <button className="promo__sign-up-button" type="button">
+          Sign Up
+        </button>
+      </div>
     </section>
   );
 }
