aaaaaa9dc63b5e5e3c7c5cbbf3ec7914ee7ec4d2
N. Lazuta <nkgavshina@gmail.com>
2022-03-10 16:26:39 +0300

    refactor: привести код к единообразному виду

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./Promo.scss";
 
-export default function Promo() {
+function Promo() {
   return (
     <section className="promo" id="overview">
       <div className="promo__background">
         <h1 className="promo__heading">CloudBudget</h1>
         <h2 className="promo__subtitle">
           Cloud budget management for everyone. Only €69.95 a Month After a 7
-          Day Trial of<br />Up to €4.99
+          Day Trial of
+          <br />
+          Up to €4.99
         </h2>
         <button className="promo__sign-up-button" type="button">
           Sign Up
@@ -17,3 +19,5 @@ export default function Promo() {
     </section>
   );
 }
+
+export default Promo;
