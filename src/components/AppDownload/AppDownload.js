b15f6aebe0e32c4293f7e554b107aa45a9a0bd82
N. Lazuta <nkgavshina@gmail.com>
2022-03-10 22:19:13 +0300

    refactor: изменить название переменной sass

--- src/components/AppDownload/AppDownload.js
+++ src/components/AppDownload/AppDownload.js
@@ -3,8 +3,9 @@ import playstore from "../../images/google-playstore-icon.svg";
 import appstore from "../../images/apple-appstore-icon.svg";
 import windows from "../../images/windows-icon.svg";
 import photo1 from "../../images/download-photo1.jpg";
-import photo2 from "../../images/download-photo1.jpg";
-import photo3 from "../../images/download-photo1.jpg";
+import photo2 from "../../images/download-photo2.jpg";
+import photo3 from "../../images/download-photo3.jpg";
+import "./AppDownload.scss";
 
 function AppDownload() {
   return (
@@ -18,53 +19,50 @@ function AppDownload() {
         </p>
         <ul className="download__links">
           <li className="download__item">
-            <a
-              className="download__link"
-              href="#"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <button className="download__link" type="button">
               <img
                 className="download__icon"
                 src={playstore}
                 alt="Google Play Icon"
               />
-            </a>
+            </button>
           </li>
           <li className="download__item">
-            <a
-              className="download__link"
-              href="#"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <button className="download__link" type="button">
               <img
                 className="download__icon"
                 src={appstore}
                 alt="App Store Icon"
               />
-            </a>
+            </button>
           </li>
           <li className="download__item">
-            <a
-              className="download__link"
-              href="#"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <button className="download__link" type="button">
               <img
                 className="download__icon"
                 src={windows}
                 alt="Windows Store Icon"
               />
-            </a>
+            </button>
           </li>
         </ul>
       </div>
       <div className="download__photo-block">
-        <img className="download__image" src={photo1} alt="Decorative picture" />
-        <img className="download__image" src={photo2} alt="Decorative picture" />
-        <img className="download__image" src={photo3} alt="Decorative picture" />
+        <img
+          className="download__image"
+          src={photo1}
+          alt="Decorative picture"
+        />
+        <img
+          className="download__image"
+          src={photo2}
+          alt="Decorative picture"
+        />
+        <img
+          className="download__image"
+          src={photo3}
+          alt="Decorative picture"
+        />
       </div>
     </section>
   );
